3360304ad665dc6e50c16ab3c1828ef04ac19944
Jose Isai Reyes Hernandez <revanisai777@gmail.com>
2022-08-09 18:20:17 +0000

    authentication func done

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -49,7 +49,20 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route POST /api/users/login
 // @access Public
 const authenticateUser = asyncHandler(async (req, res) => {
-  res.json({ msg: "User authenticated" });
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid credentials");
+  }
 });
 
 // @desc Get user data
