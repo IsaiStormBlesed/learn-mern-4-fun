63afadcfac38b9aa0f159bf3242344be23370df4
Jose Isai Reyes Hernandez <revanisai777@gmail.com>
2022-08-09 17:29:26 +0000

    user Model created

--- server/models/goalModel.js
+++ server/models/goalModel.js
@@ -3,6 +3,11 @@ const { Schema } = mongoose;
 
 const goalSchema = new Schema(
   {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
     text: {
       type: String,
       required: [true, "Please provide a text value"],
---
+++ server/models/userModel.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please provide a name"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please provide an email"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Please provide a password"],
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", userSchema);
