fa6f76f5c8d3b8333124086a15102fba44537932
Jose Isai Reyes Hernandez <revanisai777@gmail.com>
2022-08-10 16:19:35 +0000

    pages && router init && header component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
 function App() {
   return (
-    <div>
-      <h1>My Goals app</h1>
-    </div>
+    <>
+      <BrowserRouter>
+        <div className="container">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </>
   );
 }
 
